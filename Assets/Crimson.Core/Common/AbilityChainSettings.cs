967d7d157548ce510bcbea244f85882d7b99535e
totgard <saharovmax@gmail.com>
2021-09-07 16:54:54 +0300

    Add ability chain settings

---
+++ Assets/Crimson.Core/Common/AbilityChainSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Crimson.Core.Common
+{
+    [Serializable]
+    public struct AbilityChainSettings
+    {
+        [SerializeField]
+        public List<MonoBehaviour> Actions;
+        public float Delay;
+    }
+}
---
+++ Assets/Crimson.Core/Components/AbilityChainActions.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Crimson.Core.Common;
+using Crimson.Core.Common;
+using Crimson.Core.Components;
+using Crimson.Core.Utils;
+using Sirenix.OdinInspector;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Crimson.Core.Components
+{
+    [HideMonoScript]
+    public class AbilityChainActions : TimerBaseBehaviour, IActorAbility
+    {
+        public bool ExecuteOnAwake = false;
+        public AbilityChainSettings Settings = new AbilityChainSettings();
+        public IActor Actor { get; set; }
+
+        public void AddComponentData(ref Entity entity, IActor actor)
+        {
+            Actor = actor;
+        }
+
+        [ContextMenu("Test")]
+        public void Execute()
+        {
+            Timer.TimedActions.AddAction(TestMethod, 2);
+        }
+
+        private void Start()
+        {
+            if (ExecuteOnAwake)
+            {
+                Execute();
+            }
+        }
+
+        private void TestMethod()
+        {
+            Debug.Log("TestMetod");
+        }
+    }
+}
