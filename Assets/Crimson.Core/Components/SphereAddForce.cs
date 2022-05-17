0148433c35c2345349e4cec8304f48a0ca5db532
Maxim Sakharov <m.sakharov@garagewhale.com>
2022-05-17 12:51:28 +0300

    Grenade explosion
    + Demo scene
    + Crates

--- Assets/Crimson.Core/Components/SphereAddForce.cs
+++ Assets/Crimson.Core/Components/SphereAddForce.cs
@@ -1,82 +1,66 @@
-﻿using Assets.Crimson.Core.Components;
-using Crimson.Core.Common;
+﻿using Crimson.Core.Common;
 using Sirenix.OdinInspector;
-using System.Linq;
 using Unity.Entities;
 using UnityEngine;
 
 namespace Crimson.Core.Components
 {
-    public struct ExplosionForceData : IComponentData
-    {
-        public float Force;
-        public float Radius;
-        public Vector3 SourcePosition;
-    }
+	[HideMonoScript]
+	public class SphereAddForce : MonoBehaviour, IActorAbility
+	{
+		public EntityManager _dstManager;
+		public Entity _entity;
 
-    [HideMonoScript]
-    public class SphereAddForce : MonoBehaviour, IActorAbility
-    {
-        public EntityManager _dstManager;
-        public Entity _entity;
+		public float Power = 25;
+		public float Radius = 5;
+		public ForceMode Mode = ForceMode.Force;
 
-        public float Force = 25;
+		public IActor Actor { get; set; }
 
-        public float Radius = 5;
+		public void AddComponentData(ref Entity entity, IActor actor)
+		{
+			_entity = entity;
+			Actor = actor;
+			_dstManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+			if (!Actor.Abilities.Contains(this))
+			{
+				Actor.Abilities.Add(this);
+			}
+		}
 
-        public IActor Actor { get; set; }
+		public void Execute()
+		{
+			var targets = Physics.OverlapSphere(transform.position, Radius);
+			AddForceTo(targets);
+		}
 
-        public void AddComponentData(ref Entity entity, IActor actor)
-        {
-            _entity = entity;
-            Actor = actor;
-            _dstManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-            if (!Actor.Abilities.Contains(this))
-            {
-                Actor.Abilities.Add(this);
-            }
-        }
+		private void AddForceTo(Collider[] targets)
+		{
+			for (var i = 0; i < targets.Length; i++)
+			{
+				AddForceTo(targets[i]);
+			}
+		}
 
-        public void Execute()
-        {
-            var actors = GetActorsInRadius(Radius);
-            AddForceTo(actors);
-        }
+		private void AddForceTo(Collider targetCollider)
+		{
+			if (targetCollider == null || targetCollider.attachedRigidbody == null)
+			{
+				return;
+			}
 
-        private void AddForceTo(IActor[] actors)
-        {
-            for (var i = 0; i < actors.Length; i++)
-            {
-                AddForceTo(actors[i]);
-            }
-        }
+			var target = targetCollider.GetComponent<Rigidbody>();
 
-        private void AddForceTo(IActor targetActor)
-        {
-            _dstManager.AddComponent<AdditionalForceActorTag>(targetActor.ActorEntity);
-            _dstManager.AddComponentData(targetActor.ActorEntity, new ExplosionForceData
-            {
-                Force = Force,
-                Radius = Radius,
-                SourcePosition = transform.position
-            });
-            _dstManager.AddComponentData(targetActor.ActorEntity, new ActivateRagdollData());
-        }
-
-        private IActor[] GetActorsInRadius(float radius)
-        {
-            var results = Physics.OverlapSphere(transform.position, radius);
-            return results.Select(s => s.GetComponent<IActor>()).Where(s => s != null).ToArray();
-        }
-
+			target.AddExplosionForce(Power, transform.position, Radius, 3.0f, Mode);
+		}
 #if UNITY_EDITOR
 
-        private void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, Radius);
-        }
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(transform.position, Radius);
+		}
 
 #endif
-    }
+	}
 }
