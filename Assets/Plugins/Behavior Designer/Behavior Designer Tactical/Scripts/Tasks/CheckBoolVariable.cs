3ac570d8db25edb14635374aa1854ac561616461
Maxim Sakharov <m.sakharov@garagewhale.com>
2022-10-07 10:58:36 +0300

    Change bool & check bool nodes

---
+++ Assets/Plugins/Behavior Designer/Behavior Designer Tactical/Scripts/Tasks/CheckBoolVariable.cs
@@ -0,0 +1,22 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using Sirenix.OdinInspector;
+
+namespace Assets.Plugins.Behavior_Designer.Behavior_Designer_Tactical.Scripts.Tasks
+{
+	[TaskCategory("Variables")]
+	public class CheckBool : Conditional
+	{
+		[PropertyOrder(1)] public bool Value;
+		[PropertyOrder(0)] public SharedBool Variable;
+
+		public override void OnAwake()
+		{
+		}
+
+		public override TaskStatus OnUpdate()
+		{
+			return Variable.GetValue().Equals(Value) ? TaskStatus.Success : TaskStatus.Failure;
+		}
+	}
+}
