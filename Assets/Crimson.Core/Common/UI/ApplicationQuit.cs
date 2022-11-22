e6fb62b7b0fdb501dbb7819dfd24147efce71391
Maxim Sakharov <m.sakharov@garagewhale.com>
2022-11-22 19:02:28 +0300

    Add demo main menu

---
+++ Assets/Crimson.Core/Common/UI/ApplicationQuit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Crimson.Core.Common.UI
+{
+	public class ApplicationQuit : MonoBehaviour
+	{
+		public void Quit()
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/Crimson.Core/Common/UI/LoadLevelAsync.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Crimson.Core.Common.UI
+{
+	public class LoadLevelAsync : MonoBehaviour
+	{
+		[SerializeField] private UnityEvent _onLoad;
+
+		public void Load(int index)
+		{
+			SceneManager.LoadSceneAsync(index);
+			_onLoad?.Invoke();
+		}
+
+		public void Load(string name)
+		{
+			SceneManager.LoadSceneAsync(name);
+			_onLoad?.Invoke();
+		}
+	}
+}
