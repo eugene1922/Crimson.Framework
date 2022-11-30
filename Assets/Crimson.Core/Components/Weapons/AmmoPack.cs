2971e28ec7ff62fe09e134a444b179cdc95a296e
Maxim Sakharov <m.sakharov@garagewhale.com>
2022-11-30 19:13:42 +0300

    Some improvements

--- Assets/Crimson.Core/Components/Weapons/AmmoPack.cs
+++ Assets/Crimson.Core/Components/Weapons/AmmoPack.cs
@@ -8,17 +8,42 @@ namespace Assets.Crimson.Core.Components.Weapons
 	[Serializable]
 	public class AmmoPack : IAmmo
 	{
-		[ValidateInput(nameof(Validate))]
+		[ValidateInput(nameof(Validate)), OnValueChanged(nameof(ChangeTarget))]
 		public MonoBehaviour _target;
 
 		public int _value;
-		public int Value => _value;
 
 		public IHasComponentName Target => _target as IHasComponentName;
 
+		public int Value => _value;
+
+		private void ChangeTarget()
+		{
+			if (_target != null && !Validate(_target))
+			{
+				var weapon = _target.GetComponent<IWeapon>();
+				if (weapon == null)
+				{
+					var throwable = _target.GetComponent<IThrowable>();
+					if (throwable != null)
+					{
+						_target = throwable as MonoBehaviour;
+					}
+				}
+				else
+				{
+					_target = weapon as MonoBehaviour;
+				}
+			}
+		}
+
 		private bool Validate(MonoBehaviour item)
 		{
-			return item != null && item is IHasComponentName;
+			var result = item != null && (item is IWeapon || item is IThrowable);
+			if (item != null)
+			{
+			}
+			return result;
 		}
 	}
 }
